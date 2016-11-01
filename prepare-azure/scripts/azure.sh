0428c3f813c85e14d0c25009c69f4b4b0a03e859
Manesh Raveendran <manesh@manesh.me>
2016-11-01 21:22:41 +0000

    Adding stop and kill functions for trainee vms

--- prepare-azure/scripts/azure.sh
+++ prepare-azure/scripts/azure.sh
@@ -9,13 +9,79 @@ azure_display_rgs() {
 
 azure_get_instance_ips_by_rg() { 
     RG=$1 
-    need_rg $RG
 
-    az vm list-ip-addresses -g $RG -o table \
-       --query "[].{\
+    if [ -z "$RG" ]; then
+        az vm list-ip-addresses -o table \
+           --query "[].{\
+                 resourceGroup:virtualMachine.resourceGroup,\
                  name:virtualMachine.name,\
                  privateIP:virtualMachine.network.privateIpAddresses[0],\
                  publicIP:virtualMachine.network.publicIpAddresses[0].ipAddress \
                }"
+    else
+        az vm list-ip-addresses -g $RG -o table \
+           --query "[].{\
+                 resourceGroup:virtualMachine.resourceGroup,\
+                 name:virtualMachine.name,\
+                 privateIP:virtualMachine.network.privateIpAddresses[0],\
+                 publicIP:virtualMachine.network.publicIpAddresses[0].ipAddress \
+               }"
+    fi
 } 
 
+azure_start_instances_by_rg() {
+    RG=$1
+
+    if [ -z "$RG" ]; then
+        for ((i = 1; i <= $TRAINEE_COUNT; i++))
+        do
+            for ((j = 0; j < $CLUSTER_SIZE; j++))
+            do
+                echo "Starting vm trainee-vm-$j in docker-trainee-$i"
+                az vm start -g docker-trainee-$i -n trainee-vm-$j
+            done
+        done
+    else
+        for ((j = 0; j < $CLUSTER_SIZE; j++))
+        do
+            echo "Starting vm trainee-vm-$j in $RG"
+            az vm start -g $RG -n trainee-vm-$j
+        done
+    fi
+}
+
+azure_stop_instances_by_rg() {
+    RG=$1
+
+    if [ -z "$RG" ]; then
+        for ((i = 1; i <= $TRAINEE_COUNT; i++)) 
+        do
+            for ((j = 0; j < $CLUSTER_SIZE; j++))
+            do
+                echo "Deallocating vm trainee-vm-$j in docker-trainee-$i"
+                az vm deallocate -g docker-trainee-$i -n trainee-vm-$j
+            done
+        done
+    else
+        for ((j = 0; j < $CLUSTER_SIZE; j++))
+        do
+            echo "Deallocating vm trainee-vm-$j in $RG"
+            az vm deallocate -g $RG -n trainee-vm-$j
+        done
+    fi
+}
+
+azure_kill_instances_by_rg() {
+    RG=$1
+
+    if [ -z "$RG" ]; then
+        for ((i = 1; i <= $TRAINEE_COUNT; i++))
+        do
+            echo "Deleting resource group docker-trainee-$i"
+            az resource group delete -n docker-trainee-$i
+        done
+    else
+        echo "Deleting resource group $RG"
+        az resource group delete -n $RG
+    fi
+}
